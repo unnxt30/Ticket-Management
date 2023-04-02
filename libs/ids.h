d9c89f8a8a26b9e24e585ef417d1488d8a451805
unnxt30 <105226341+unnxt30@users.noreply.github.com>
2023-04-02 22:27:12 +0530

    Updated ids.h to generate ref_id using checksum method

--- libs/ids.h
+++ libs/ids.h
@@ -1,37 +1,31 @@
 // Limiting the event to a 100 Tickets only.
-#define EVENT_SIZE 100
 
-
-int ids[EVENT_SIZE];
-
-//checks if the id is present in the Booked tickets.
-int is_id(int ref_id, int ids[])
+int toNum(char c)
 {
-    for (int i = 0; i<EVENT_SIZE; i++)
-    {
-        if(ref_id == ids[i])
-        {
-            return 1;
-        }
-    }
-
-    return 0;
+    int i = c -'0' ;
+    return i;
 }
 
 //Generates a unique ref_id for each ticket.
-void *generate_id(int ids[])
+int generate_id()
 {
 
-    srand(time(NULL)); // seed the random number generator with the current time
+    // srand(time(NULL)); // seed the random number generator with the current time
     
-    // create an array to hold the n random numbers
-    ids[0] = rand() % (100 - 0 + 1) + 0; // generate the first random number
+    int num = rand() % (999 - 100 + 1) + 100; // generate first three digit of ID
     
-    for (int i = 1; i < EVENT_SIZE; i++) {
-        ids[i] = ids[i-1]; // initialize the next number to the previous number
-        while (ids[i] == ids[i-1]) { // loop until the next number is different from the previous number
-            ids[i] = rand() % (100 - 0 + 1) + 0; // generate a new random number
-        }
+    char str[3];
+    int twoSum = 0;
+
+    sprintf(str, "%i", num); //convert number to string
+
+    for(int i = 0; i<3; i++)
+    {
+        twoSum += toNum(str[i]);
     }
+
+    num = (num*100) + twoSum;
+
+    return num;
 
 }
